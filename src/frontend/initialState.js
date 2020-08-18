933a194c62e2735512e8ab39c0e5d97a56d60a14
hugolperez <hperez.hugoleonel@gmail.com>
2020-08-17 22:50:51 -0600

    ESLint configured

--- src/frontend/initialState.js
+++ src/frontend/initialState.js
@@ -14,7 +14,7 @@ const initialState = {
       'duration': 164,
       'cover': 'http://dummyimage.com/800x600.png/99118E/ffffff',
       'description': 'Vestibulum ac est lacinia nisi venenatis tristique',
-      'source': 'https://mdstrm.com/video/58333e214ad055d208427db5.mp4'
+      'source': 'https://mdstrm.com/video/58333e214ad055d208427db5.mp4',
     },
     {
       'id': 3,
@@ -27,7 +27,7 @@ const initialState = {
       'duration': 137,
       'cover': 'http://dummyimage.com/800x600.png/302140/ffffff',
       'description': 'Vestibulum ac est lacinia nisi venenatis tristique',
-      'source': 'https://mdstrm.com/video/58333e214ad055d208427db5.mp4'
+      'source': 'https://mdstrm.com/video/58333e214ad055d208427db5.mp4',
     },
     {
       'id': 4,
@@ -40,7 +40,7 @@ const initialState = {
       'duration': 163,
       'cover': 'http://dummyimage.com/800x600.png/5472FF/ffffff',
       'description': 'Vestibulum ac est lacinia nisi venenatis tristique',
-      'source': 'https://mdstrm.com/video/58333e214ad055d208427db5.mp4'
+      'source': 'https://mdstrm.com/video/58333e214ad055d208427db5.mp4',
     },
     {
       'id': 5,
@@ -53,7 +53,7 @@ const initialState = {
       'duration': 194,
       'cover': 'http://dummyimage.com/800x600.png/B36F20/ffffff',
       'description': 'Vestibulum ac est lacinia nisi venenatis tristique',
-      'source': 'https://mdstrm.com/video/58333e214ad055d208427db5.mp4'
+      'source': 'https://mdstrm.com/video/58333e214ad055d208427db5.mp4',
     },
     {
       'id': 6,
@@ -66,7 +66,7 @@ const initialState = {
       'duration': 124,
       'cover': 'http://dummyimage.com/800x600.png/CCC539/ffffff',
       'description': 'Vestibulum ac est lacinia nisi venenatis tristique',
-      'source': 'https://mdstrm.com/video/58333e214ad055d208427db5.mp4'
+      'source': 'https://mdstrm.com/video/58333e214ad055d208427db5.mp4',
     },
     {
       'id': 7,
@@ -79,8 +79,8 @@ const initialState = {
       'duration': 179,
       'cover': 'http://dummyimage.com/800x600.png/FF7A90/ffffff',
       'description': 'Vestibulum ac est lacinia nisi venenatis tristique',
-      'source': 'https://mdstrm.com/video/58333e214ad055d208427db5.mp4'
-    }
+      'source': 'https://mdstrm.com/video/58333e214ad055d208427db5.mp4',
+    },
   ],
   'originals': [
     {
@@ -94,7 +94,7 @@ const initialState = {
       'duration': 148,
       'cover': 'http://dummyimage.com/800x600.png/306880/ffffff',
       'description': 'Vestibulum ac est lacinia nisi venenatis tristique',
-      'source': 'https://mdstrm.com/video/58333e214ad055d208427db5.mp4'
+      'source': 'https://mdstrm.com/video/58333e214ad055d208427db5.mp4',
     },
     {
       'id': 9,
@@ -107,7 +107,7 @@ const initialState = {
       'duration': 128,
       'cover': 'http://dummyimage.com/800x600.png/604180/ffffff',
       'description': 'Vestibulum ac est lacinia nisi venenatis tristique',
-      'source': 'https://mdstrm.com/video/58333e214ad055d208427db5.mp4'
+      'source': 'https://mdstrm.com/video/58333e214ad055d208427db5.mp4',
     },
     {
       'id': 10,
@@ -120,7 +120,7 @@ const initialState = {
       'duration': 346,
       'cover': 'http://dummyimage.com/800x600.png/FF91BA/ffffff',
       'description': 'Vestibulum ac est lacinia nisi venenatis tristique',
-      'source': 'https://mdstrm.com/video/58333e214ad055d208427db5.mp4'
+      'source': 'https://mdstrm.com/video/58333e214ad055d208427db5.mp4',
     },
     {
       'id': 11,
@@ -133,7 +133,7 @@ const initialState = {
       'duration': 226,
       'cover': 'http://dummyimage.com/800x600.png/45807C/ffffff',
       'description': 'Vestibulum ac est lacinia nisi venenatis tristique',
-      'source': 'https://mdstrm.com/video/58333e214ad055d208427db5.mp4'
+      'source': 'https://mdstrm.com/video/58333e214ad055d208427db5.mp4',
     },
     {
       'id': 12,
@@ -146,7 +146,7 @@ const initialState = {
       'duration': 190,
       'cover': 'http://dummyimage.com/800x600.png/577380/ffffff',
       'description': 'Vestibulum ac est lacinia nisi venenatis tristique',
-      'source': 'https://mdstrm.com/video/58333e214ad055d208427db5.mp4'
+      'source': 'https://mdstrm.com/video/58333e214ad055d208427db5.mp4',
     },
     {
       'id': 13,
@@ -159,7 +159,7 @@ const initialState = {
       'duration': 160,
       'cover': 'http://dummyimage.com/800x600.png/5472FF/ffffff',
       'description': 'Vestibulum ac est lacinia nisi venenatis tristique',
-      'source': 'https://mdstrm.com/video/58333e214ad055d208427db5.mp4'
+      'source': 'https://mdstrm.com/video/58333e214ad055d208427db5.mp4',
     },
   ],
 };
--- src/frontend/routes/App.js
+++ src/frontend/routes/App.js
@@ -19,6 +19,6 @@ const App = () => (
       </Switch>
     </Layout>
   </BrowserRouter>
-)
+);
 
 export default App;
